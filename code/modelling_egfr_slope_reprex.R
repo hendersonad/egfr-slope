5b9f17fd3e637ba44f88817bdfd77ffa5c1a2cf4
Alasdair Henderson <alasdair.henderson@glasgow.ac.uk>
2025-04-22 22:25:27 +0100

    Update modelling_egfr_slope_reprex.R

--- code/modelling_egfr_slope_reprex.R
+++ code/modelling_egfr_slope_reprex.R
@@ -54,7 +54,7 @@ path <- sub("/code$", "", dirname(rstudioapi::getSourceEditorContext()$path))
 # value according to the available date in your trial. For example, in DAPA-CKD, 
 # the first post-randomisation visit is at 14 days (2 weeks), so `k` would be 
 # set to 14
-k <- 21
+k <- 14
 
 
 # Source functions --------------------------------------------------------
@@ -65,8 +65,8 @@ source(glue::glue("{path}/src/compute_slope.R"))
 
 # Load data ---------------------------------------------------------------
 
-bl <- generate_synthetic_data(.table = "baseline")
-fu <- generate_synthetic_data(.table = "follow-up")
+synth_bl <- generate_synthetic_data(.table = "baseline")
+synth_fu <- generate_synthetic_data(.table = "follow-up")
 
 # If you encounter issues running the function above, the two datasets generated 
 # by this function are also available in CSV format in the `data` sub-directory 
@@ -75,6 +75,11 @@ fu <- generate_synthetic_data(.table = "follow-up")
 # bl <- readr::read_csv("{path}/data/synthetic_trial_baseline.csv")
 # fu <- readr::read_csv("{path}/data/synthetic_trial_egfr_follow_up.csv")
 
+bl <- haven::read_dta("/Volumes/JHund/DAPA-HF/SMART-C/slope_bl.dta")
+bl$hfhospn = factor(bl$hfhospn, labels = c("Y", "N"))
+
+fu <- haven::read_dta("/Volumes/JHund/DAPA-HF/SMART-C/slope_fu.dta")
+fu
 
 # Prepare data ------------------------------------------------------------
 
@@ -83,16 +88,19 @@ fu <- generate_synthetic_data(.table = "follow-up")
 # optional. Depending on your study design, you might prefer an 
 # intention-to-treat population instead
 arm <- bl |> 
-  dplyr::filter(trtfl == "Y") |> 
-  dplyr::select(usubjid, trt01pn, blglp1, strata) |> 
-  dplyr::mutate(blglp1 = dplyr::if_else(blglp1 == "Y", 1, 0))
+  dplyr::select(usubjid, trt01pn = trtpn, egfrgr1, t2dblfl, stratan, hfhospn, acearbarni, t2dblfl) |> 
+  dplyr::mutate(
+    egfrgr1 = factor(
+      egfrgr1,
+      levels = c("< 30", "30 and < 45", "45 and < 60", ">= 60")
+      )
+  )
 
 # Repeat eGFR
 # Here, we filter for on-treatment measurements and those flagged for analysis. 
 # Adjust these criteria according to your chosen analysis strategy
 gfr <- fu |> 
-  dplyr::filter(dplyr::if_all(c(trtfl, anl01fl), \(x) x == "Y")) |> 
-  dplyr::distinct(usubjid, aval, ady, base, avisitn)
+  dplyr::distinct(usubjid, aval,ady, base, avisitn)
 
 # Join
 gfr_c <- arm |> 
@@ -104,6 +112,9 @@ gfr_c <- arm |>
     spline = dplyr::if_else(time >= k / 365.25, time - k / 365.25, 0)
   )
 
+
+gfr_c$stratan <- factor(gfr_c$stratan)
+
 # Formatting the data for the model is likely the most crucial step. The 
 # synthetic data used in this reprex should give you an idea of the required 
 # format, but consult the repository README if you're unsure about anything or 
@@ -114,7 +125,7 @@ gfr_c <- arm |>
 
 # Fit mixed effects model with unstructured residual variance-covariance matrix
 fit <- lme4::lmer(
-  aval ~ base + strata + time * trt01pn + spline * trt01pn - 1 + 
+  aval ~ base + stratan + hfhospn + time * trt01pn + spline * trt01pn - 1 + 
   (time | usubjid), data = gfr_c
 )
 
@@ -131,7 +142,8 @@ fit <- lme4::lmer(
 # this example, 1095.75 represents the total trial follow-up period (roughly 3 
 # years in days). By subtracting 21 days (acute slope), we calculate the
 # fraction that remains for the chronic slope
-prop <- (1095.75 - k) / 1095.75
+maxfup <- 835 # max(t2dth) in DAPA
+prop <- (835 - k) / 835
 
 # Compute eGFR slope for the whole population
 all <- compute_slope(
@@ -143,53 +155,81 @@ all <- compute_slope(
   .output = "all"
 )
 
+all
 
 # Fit model and compute slope by binary subgroup variable -----------------
 
 # Recode baseline GLP-1RA use as factor and set `blglp1` == "Yes" as reference
 gfr_c <- gfr_c  |> 
   dplyr::mutate(
-    blglp1 = factor(blglp1, levels = c(1, 0), labels = c("Yes", "No"))
+    acearbarni = factor(acearbarni, levels = c(1, 0), labels = c("Yes", "No")),
+    t2dblfl = factor(t2dblfl, levels = c("Y", "N"), labels = c("Yes", "No"))
   )
 
 # Fit mixed effects model with unstructured residual variance-covariance matrix
-# this time with adjustment and interactions with `blglp1`
-fit_binary <- lme4::lmer(
-  aval ~ base + strata + time * trt01pn + spline * trt01pn + time * blglp1 +
-  spline * blglp1 + trt01pn * blglp1 + time * trt01pn * blglp1 + 
-  spline * trt01pn * blglp1 - 1 + (time | usubjid), data = gfr_c
+# this time with adjustment and interactions with `acearbarni`
+fit_binary_rasblock <- lme4::lmer(
+  aval ~ base + stratan + hfhospn + time * trt01pn + spline * trt01pn + time * acearbarni +
+  spline * acearbarni + trt01pn * acearbarni + time * trt01pn * acearbarni + 
+  spline * trt01pn * acearbarni - 1 + (time | usubjid), data = gfr_c
+)
+
+fit_binary_diabetes <- lme4::lmer(
+  aval ~ base + stratan + hfhospn + time * trt01pn + spline * trt01pn + time * t2dblfl +
+    spline * t2dblfl + trt01pn * t2dblfl + time * trt01pn * t2dblfl + 
+    spline * trt01pn * t2dblfl - 1 + (time | usubjid), data = gfr_c
 )
 
 # Compute eGFR slope by binary subgroups (baseline GLP-1RA use)
-binary_subgroups <- compute_slope(
-  .model_obj = fit_binary, 
+total_ras <- compute_slope(
+  .model_obj = fit_binary_rasblock, 
   .time_var = "time", 
   .intervention_var = "trt01pn", 
   .spline_var = "spline", 
   .prop = prop, 
-  .by = "blglp1", # `.by` argument specified with subgroup variable
+  .by = "acearbarni", # `.by` argument specified with subgroup variable
   .output = "total"
-)
+) |> print()
+
+chronic_ras <- compute_slope(
+  .model_obj = fit_binary_rasblock, 
+  .time_var = "time", 
+  .intervention_var = "trt01pn", 
+  .spline_var = "spline", 
+  .prop = prop, 
+  .by = "acearbarni", # `.by` argument specified with subgroup variable
+  .output = "chronic"
+) |> print()
+
+total_t2db <- compute_slope(
+  .model_obj = fit_binary_diabetes, 
+  .time_var = "time", 
+  .intervention_var = "trt01pn", 
+  .spline_var = "spline", 
+  .prop = prop, 
+  .by = "t2dblfl", # `.by` argument specified with subgroup variable
+  .output = "total"
+) |> print()
+
+total_t2db <- compute_slope(
+  .model_obj = fit_binary_diabetes, 
+  .time_var = "time", 
+  .intervention_var = "trt01pn", 
+  .spline_var = "spline", 
+  .prop = prop, 
+  .by = "t2dblfl", # `.by` argument specified with subgroup variable
+  .output = "chronic"
+) |> print()
 
 
 # Fit model and compute slope by multi-level subgroup variable ------------
 
-# Generate an ordinal categorical variable for baseline eGFR subgroups. The 
-# `compute_slope()` function can handle any number of levels, but four are 
-# demonstrated here as an example
-gfr_c <- gfr_c |> 
-  mutate(
-    gfr_grp = factor(ntile(base, 4), levels = c(1:4), labels = c(1:4)),
-    # Set quartile 4 as the reference level
-    gfr_grp = forcats::fct_relevel(gfr_grp, "4")
-  )
-
 # Fit mixed effects model with unstructured residual variance-covariance matrix
 # with adjustment and interactions with `gfr_grp`
 fit_ordinal <- lme4::lmer(
-  aval ~ base + strata + time * trt01pn + spline * trt01pn + time * gfr_grp +
-  spline * gfr_grp + trt01pn * gfr_grp + time * trt01pn * gfr_grp + 
-  spline * trt01pn * gfr_grp - 1 + (time | usubjid), data = gfr_c
+  aval ~ base + stratan + hfhospn + time * trt01pn + spline * trt01pn + time * egfrgr1 +
+  spline * egfrgr1 + trt01pn * egfrgr1 + time * trt01pn * egfrgr1 + 
+  spline * trt01pn * egfrgr1 - 1 + (time | usubjid), data = gfr_c
 )
 
 # Compute eGFR slope by multiple subgroups (baseline eGFR quartiles)
@@ -199,6 +239,81 @@ ordinal_subgroups <- compute_slope(
   .intervention_var = "trt01pn", 
   .spline_var = "spline",
   .prop = prop, 
-  .by = "gfr_grp", # `.by` argument specified with subgroup variable
+  .by = "egfrgr1", # `.by` argument specified with subgroup variable
   .output = "total"
+) |> print()
+
+chronic_gfrgroup <- compute_slope(
+  .model_obj = fit_ordinal, 
+  .time_var = "time", 
+  .intervention_var = "trt01pn", 
+  .spline_var = "spline",
+  .prop = prop, 
+  .by = "egfrgr1", # `.by` argument specified with subgroup variable
+  .output = "chronic"
+) |> print()
+
+
+
+
+# egfr by diabetes --------------------------------------------------------
+gfr_c_nodiab <- gfr_c |> 
+  filter(t2dblfl == "No", egfrgr1 != "< 30")
+
+gfr_c_diab <- gfr_c |> 
+  filter(t2dblfl == "Yes", egfrgr1 != "< 30")
+
+db_egfr <- lme4::lmer(
+  aval ~ base + stratan + hfhospn + time * trt01pn + spline * trt01pn + time * egfrgr1 +
+    spline * egfrgr1 + trt01pn * egfrgr1 + time * trt01pn * egfrgr1 + 
+    spline * trt01pn * egfrgr1 - 1 + (time | usubjid), data = gfr_c_diab
 )
+
+no_db_egfr <- lme4::lmer(
+  aval ~ base + stratan + hfhospn + time * trt01pn + spline * trt01pn + time * egfrgr1 +
+    spline * egfrgr1 + trt01pn * egfrgr1 + time * trt01pn * egfrgr1 + 
+    spline * trt01pn * egfrgr1 - 1 + (time | usubjid), data = gfr_c_nodiab
+)
+  
+# Compute eGFR slope by multiple subgroups (baseline eGFR quartiles)
+compute_slope(
+  .model_obj = db_egfr, 
+  .time_var = "time", 
+  .intervention_var = "trt01pn", 
+  .spline_var = "spline",
+  .prop = prop, 
+  .by = "egfrgr1", # `.by` argument specified with subgroup variable
+  .output = "total"
+) |> print()
+
+compute_slope(
+  .model_obj = db_egfr, 
+  .time_var = "time", 
+  .intervention_var = "trt01pn", 
+  .spline_var = "spline",
+  .prop = prop, 
+  .by = "egfrgr1", # `.by` argument specified with subgroup variable
+  .output = "chronic"
+) |> print()  
+
+
+# Compute eGFR slope by multiple subgroups (baseline eGFR quartiles)
+compute_slope(
+  .model_obj = no_db_egfr, 
+  .time_var = "time", 
+  .intervention_var = "trt01pn", 
+  .spline_var = "spline",
+  .prop = prop, 
+  .by = "egfrgr1", # `.by` argument specified with subgroup variable
+  .output = "total"
+) |> print()
+
+compute_slope(
+  .model_obj = no_db_egfr, 
+  .time_var = "time", 
+  .intervention_var = "trt01pn", 
+  .spline_var = "spline",
+  .prop = prop, 
+  .by = "egfrgr1", # `.by` argument specified with subgroup variable
+  .output = "chronic"
+) |> print()  
